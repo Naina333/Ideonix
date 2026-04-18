b72bc85d4cbc75ea0012f610380231593af69b30
Naina Shri Gupta <nsgupta2712@gmail.com>
2026-04-18 23:12:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+VANTA.NET({
+    el: "#vanta-bg",
+    color: 0x8b5e3c,
+    backgroundColor: 0xeae3d9,
+    points: 10,
+    maxDistance: 20,
+    spacing: 18
+});
+
+function toggleDark() {
+    document.body.classList.toggle("dark");
+}
+
